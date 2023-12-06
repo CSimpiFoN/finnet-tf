6c57e5420913483999e82c6eb03cb6f316d7f990
Tamas Juhasz <tamas.juhasz1@williamhill.com>
2023-12-06 14:28:39 +0100

    Change - S3 and CF code separated into 2 TFs

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "domain_name_1" {
+  value = module.cloudfront_1.domain_name
+}
+
+output "domain_name_2" {
+  value = module.cloudfront_2.domain_name
+}
+
+output "domain_name_3" {
+  value = module.cloudfront_3.domain_name
+}
