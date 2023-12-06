c7e14234ea9ea9b2415f8955245e4cf9e80441b4
Tamas Juhasz <tamas.juhasz1@williamhill.com>
2023-12-06 14:26:16 +0100

    Change - Provider versions added

--- modules/aws_cloudfront_distribution/providers.tf
+++ modules/aws_cloudfront_distribution/providers.tf
@@ -1,13 +1,16 @@
 terraform {
+  required_version = ">= 1.6.4"
+
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = ">= 5.29.0"
     }
   }
 }
 
 provider "aws" {
   default_tags {
-    tags = merge({Name: var.domain_name},{Environment: var.environment}, var.additional_tags)
+    tags = merge({Name: var.path},{Environment: var.environment}, var.additional_tags)
   }
 }
